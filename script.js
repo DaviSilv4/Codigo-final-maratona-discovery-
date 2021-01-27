4fda31bf5ee2dd36cb9dedd6b941c6b40226ce06
DaviSilv4 <david.br963@gmail.com>
2021-01-27 10:44:44 -0300

    Projeto Dev Finances

---
+++ script.js
@@ -0,0 +1,9 @@
+const modal = {
+    open(){
+        let abrir = document.querySelector('.modal-overlay').classList.add('active')
+        
+    },
+    close(){
+        let fechar = document.querySelector('.modal-ovelay').classList.remove('active')
+    }
+}
